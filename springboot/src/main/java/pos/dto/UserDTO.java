1fbc44cf8759079c576355388461bb833f015dd4
Enos Francisco <enosfrancisco@gmail.com>
2020-06-25 15:02:52 -0300

    valitadions in backend

---
+++ springboot/src/main/java/pos/dto/UserDTO.java
@@ -0,0 +1,79 @@
+package pos.dto;
+
+import pos.model.Phone;
+import pos.model.Role;
+import pos.model.User;
+
+import java.util.List;
+
+public class UserDTO {
+
+    private String name;
+
+//    private String cpf;
+
+    private String email;
+
+    private String password;
+
+//    private Role role;
+//
+    private List<Phone> phone;
+
+    //    public User parserToUser() {
+//        return new User(name, cpf, email, role, password, phone);
+//    }
+    public User parserToUser() {
+        String password = this.password;
+        this.setPassword("");
+        return new User(name, email, password, phone);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public String getCpf() {
+//        return cpf;
+//    }
+//
+//    public void setCpf(String cpf) {
+//        this.cpf = cpf;
+//    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+//    public Role getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(Role role) {
+//        this.role = role;
+//    }
+//
+    public List<Phone> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(List<Phone> phone) {
+        this.phone = phone;
+    }
+}
--- springboot/src/main/java/pos/service/UserService.java
+++ springboot/src/main/java/pos/service/UserService.java
@@ -16,62 +16,74 @@ import pos.repository.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService {
-	
-	@Autowired
-	private UserRepository userRepository;
-		
-		public List<User> users() {						
-				List<User> users =  (List<User>) userRepository.findAll();		
-				return users;
- 		}
-		
-		
-		public User userEmail(@PathVariable (value = "emailUser") String email) {			
-				return userRepository.findUserbyEmail(email);
-	 	}
-	
-		
-		
-		public Optional<User> userWithId(@PathVariable (value = "idUser") Long id) {			
-			   Optional<User> user =  userRepository.findById(id);		
-				return user;
-	 	}
-		
-		public User storeUser( @RequestBody User user) {
-			return userRepository.save(user);			
-		}
-		
-		public User updateUser( @RequestBody User user) {
-			return userRepository.save(user);			
-		}
-		
-		public void deleteUser( @PathVariable (value = "idUser") Long id 
-				) {
-			 userRepository.deleteById(id);
-			return ;			
-		}
-		
-		
-		//consultar usuario no banco
-		@Override
-		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-						
-			User user = userRepository.findUserbyEmail(username);
-			
-			if (user == null) {
-				throw new UsernameNotFoundException("Usuário não encontrado");
-			}
-			// caso esteja correta, já retorna email, password e autorizações
-			// poderia retonar, apenas o user, mas retornando um new user, ele já retorna com alguns padrões 
-			// do propio framework com validações
-			return new org.springframework.security.core.userdetails.User(
-					user.getEmail(),
-					user.getPassword(),
-					user.getAuthorities());
-			//
-			}
 
+    @Autowired
+    private UserRepository repository;
 
-		
+    protected UserRepository getRepository() {
+        return repository;
+    }
 
+    public List<User> users() {
+        List<User> users = (List<User>) repository.findAll();
+        return users;
+    }
+
+    public User userEmail(String email) {
+        return repository.findUserbyEmail(email);
+    }
+
+    public Optional<User> userWithId(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user;
+    }
+
+    private void validateCreate(User model) throws Exception {
+        if (getRepository().findUserbyEmail(model.getEmail()) != null) {
+            throw new Exception("email.exists");
+        }
+        if (getRepository().findByEmailAddress(model.getEmail()) != null) {
+            throw new Exception("email.exists");
+        }
+//		if (getRepository().findByCpf(model.getCpf()) != null) {
+//			throw new Exception("cpf.exists");
+//		}
+        if (model.getName().length() < 3) {
+            throw new Exception("name.invalid");
+        }
+    }
+
+    public User update(User model) throws Exception {
+        validateCreate(model);
+        return repository.save(model);
+    }
+
+    public User create(User model) throws Exception {
+        validateCreate(model);
+        return repository.save(model);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+        return;
+    }
+
+    //consultar usuario no banco
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = repository.findUserbyEmail(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+        // caso esteja correta, já retorna email, password e autorizações
+        // poderia retonar, apenas o user, mas retornando um new user, ele já retorna com alguns padrões
+        // do propio framework com validações
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getAuthorities());
+        //
+    }
 }
